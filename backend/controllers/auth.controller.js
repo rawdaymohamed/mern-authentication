b052812663a39d9116bfc2297aed386bf19dd8b2
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-30 03:35:45 +0200

    Feat: backend structure

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const register = async (req, res) => {
+    res.json({ message: "Register route" })
+}
+export const login = async (req, res) => {
+    res.json({ message: "Login route" })
+}
+export const logout = async (req, res) => {
+    res.json({ message: "Logout route" })
+}
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js"
 dotenv.config()
 const app = express();
+const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => { res.json({ message: "MERN Auth" }) })
-app.listen(4000, async () => {
+app.use("/api/auth", authRoutes);
+app.listen(PORT, async () => {
     connectDB()
-    console.log("Server is running on port 4000")
+    console.log(`Server is running on port ${PORT}`)
 })
