0225778ea6ce29526c1b154c2808fd4a68eac3d8
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-30 03:06:08 +0200

    Feat: Add mongodb connnect function

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+dotenv.config()
 const app = express();
 app.get("/", (req, res) => { res.json({ message: "MERN Auth" }) })
-app.listen(4000, () => console.log("Server is running on port 4000"))
+app.listen(4000, async () => {
+    connectDB()
+    console.log("Server is running on port 4000")
+})
