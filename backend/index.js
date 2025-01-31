e3ecc1ab3518d0ddc913d38b310d40c81b624453
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-02-01 01:03:28 +0200

    Fix: Remove async from app listen

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => { res.json({ message: "MERN Auth" }) })
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
     connectDB()
     console.log(`Server is running on port ${PORT}`)
 })
